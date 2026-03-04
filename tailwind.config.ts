e5b14d115e4645f715962628455f3cb38dd7041f
Craig Allen <craigallen21@gmail.com>
2026-03-03 23:22:38 -0500

    font size
    updated font sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,18 @@ export default {
       },
     },
     extend: {
+      fontSize: {
+        xs: "1rem",
+        sm: "1rem",
+        base: "1rem",
+        lg: "1.125rem",
+        xl: "1.25rem",
+        "2xl": "1.5rem",
+        "3xl": "1.875rem",
+        "4xl": "2.25rem",
+        "5xl": "3rem",
+        "6xl": "3.75rem",
+      },
       fontFamily: {
         sans: ["DM Sans", "system-ui", "sans-serif"],
         serif: ["DM Serif Display", "Georgia", "serif"],
